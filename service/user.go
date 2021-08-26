4e6cd6975eba4fdf2b8190cf1e4799580b6ec962
Gennady Novik <novik@socialreport.com>
2021-08-26 21:54:10 +0300

    small improve

--- service/user.go
+++ service/user.go
@@ -7,17 +7,24 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+const INVALID_USERNAME_PASSWORD string = "Invalid email or password"
+
 func Authenticate(email string, password string) (*model.User, error) {
 
 	user := new(model.User)
 
 	if res := dao.DB.Where(&model.User{Email: email}).First(&user); res.RowsAffected <= 0 {
-		err := errors.New("Invalid Credentials")
+		err := errors.New(INVALID_USERNAME_PASSWORD)
 		return nil, err
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		err = errors.New("Invalid Credentials")
+		err = errors.New(INVALID_USERNAME_PASSWORD)
+		return nil, err
+	}
+
+	if user.Deleted != nil {
+		err := errors.New(INVALID_USERNAME_PASSWORD)
 		return nil, err
 	}
 
