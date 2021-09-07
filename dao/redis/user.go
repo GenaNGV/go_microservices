dc897720b16b8e80bc8fb1b69dc720cab76a849f
Gennady Novik <novik@socialreport.com>
2021-09-07 19:50:03 +0300

    temporary

--- dao/redis/user.go
+++ dao/redis/user.go
@@ -3,15 +3,28 @@ package redis
 import (
 	"auth/enviroment"
 	"auth/model"
+	"encoding/json"
 	"errors"
 	"time"
 )
+
+var ErrUserNotFound = errors.New("user not found")
 
 func Put(u *model.UserAuth) {
 	enviroment.Env.RDB.Set(u.Token, u, time.Duration(u.Expired.Unix()))
 }
 
 func Get(token string) (*model.UserAuth, error) {
-	err := errors.New("not implemented")
-	return nil, err
+
+	val := enviroment.Env.RDB.Get(token)
+	if val == nil {
+		return nil, ErrUserNotFound
+	} else {
+		userAuth := &model.UserAuth{}
+		err := json.Unmarshal([]byte(val.Val()), &userAuth)
+		if err != nil {
+			return nil, err
+		}
+		return userAuth, nil
+	}
 }
