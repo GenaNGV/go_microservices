40889aac1b41fb852051c871169fda4aaa5fc051
Gennady Novik <novik@socialreport.com>
2021-09-24 18:18:03 +0300

    next variant

--- service/parse.go
+++ service/parse.go
@@ -18,7 +18,7 @@ var (
 	ErrMissedFile = errors.New("missed file")
 )
 
-func Parse(fileName string, terms []string, user *model.UserAuth) (*model.JobInfo, error) {
+func Parse(fileName string, terms []string, user *model.UserAuth, rule string) (*model.JobInfo, error) {
 
 	jobInfo := pg.CreateJob(fileName, user.Id)
 
@@ -39,15 +39,19 @@ func Parse(fileName string, terms []string, user *model.UserAuth) (*model.JobInf
 		return nil, ErrMissedFile
 	}
 
-	go runJob(jobInfo, terms)
+	jobInfo.Status = utils.Processing
+	enviroment.Env.DB.Save(jobInfo)
+
+	if "concurrent" == rule {
+		go runConcurrentJob(jobInfo, terms)
+	} else {
+		go runJob(jobInfo, terms)
+	}
 
 	return jobInfo, nil
 }
 
 func runJob(jobInfo *model.JobInfo, terms []string) {
-
-	jobInfo.Status = utils.Processing
-	enviroment.Env.DB.Save(jobInfo)
 
 	log.WithFields(log.Fields{"file": jobInfo.FileName, "job": *jobInfo.Id}).Info("Parsing")
 
@@ -70,6 +74,62 @@ func runJob(jobInfo *model.JobInfo, terms []string) {
 	wg.Wait()
 
 	log.WithFields(log.Fields{"job": *jobInfo.Id}).Info("Saving to database")
+	finishJob(jobInfo, utils.Finished)
+}
+
+func runConcurrentJob(jobInfo *model.JobInfo, terms []string) {
+
+	log.WithFields(log.Fields{"file": jobInfo.FileName, "job": *jobInfo.Id}).Info("Parsing concurrent")
+
+	f, err := os.Open(jobInfo.FileName)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	scanner := bufio.NewScanner(f)
+
+	channel := make(chan int)
+
+	total := 0
+
+	var wg sync.WaitGroup
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		for i := 0; i < len(terms); i++ {
+			wg.Add(1)
+
+			term := terms[i]
+
+			go func() {
+				//defer wg.Done()
+				log.WithFields(log.Fields{"job": *jobInfo.Id, "term": term, "line": line}).Info("...")
+
+				findConcurrentTerm(term, channel, line)
+			}()
+		}
+	}
+
+	go func(c chan int) {
+		wg.Wait()
+
+		close(c)
+	}(channel)
+
+	for v := range channel {
+		log.Fatal(v)
+	}
+	//rowValue := <- c
+	//total += rowValue
+
+	log.WithFields(log.Fields{"job": *jobInfo.Id, "total": total}).Info("Saving to database")
 	finishJob(jobInfo, utils.Finished)
 }
 
@@ -114,6 +174,22 @@ func findTerm(jobInfo *model.JobInfo, term string, channel chan int) {
 	log.WithFields(log.Fields{"job": *jobInfo.Id, "term": term, "found": jobStatics.Count}).Info("Processed")
 
 	pg.SaveJobStatics(jobStatics)
+}
+
+func findConcurrentTerm(term string, channel chan<- int, line string) {
+
+	total := 0
+
+	index := strings.Index(line, term)
+
+	for index >= 0 {
+		total++
+
+		line = line[index+1:]
+		index = strings.Index(line, term)
+	}
+
+	channel <- total
 }
 
 func finishJob(jobInfo *model.JobInfo, status utils.JobStatus) {
